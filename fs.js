80d2f12a77f3088a45ee532b218012488fb3b68f
Syed zain ali shah <zaingit780@gmail.com>
2025-01-25 13:27:16 +0500

    Add fs.js to demonstrate synchronous and asynchronous file reading, and create file.txt with sample content

---
+++ fs.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+console.log("First");
+const fileContent = fs.readFileSync("./file.txt", "utf-8");
+console.log({ fileContent });
+
+console.log("Second");
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log({ err });
+  } else {
+    console.log({ data });
+  }
+});
+
+console.log("Third");
