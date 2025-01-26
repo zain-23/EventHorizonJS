610bbf35b73d0cac0fc0ba44d2387ff14111a49f
ZainKodexo <syed.zain@kodexolabs.com>
2025-01-26 10:24:42 +0500

    Add http.js to implement a simple HTTP server that responds with "Hello, World!"

---
+++ http.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!");
+});
+
+server.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
