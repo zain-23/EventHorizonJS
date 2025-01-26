9392982c2b721a343e779a38ce43735dbdf62396
zain-23 <zaingit780@gmail.com>
2025-01-26 12:23:05 +0500

    Adjust threadpool.js to set UV_THREADPOOL_SIZE to 6 and update MAX_CALLS to 6 for improved performance

--- threadpool.js
+++ threadpool.js
@@ -4,10 +4,10 @@ const crypto = require("crypto");
 // const start = Date.now();
 // crypto.pbkdf2Sync("a", "b", 100000, 512, "sha512");
 // console.log("1:", Date.now() - start);
-
+process.env.UV_THREADPOOL_SIZE = 6;
 // Async version
-const MAX_CALLS = 10;
-
+const MAX_CALLS = 6;
+// libuv threadpool has 4 threads by default
 const start = Date.now();
 
 for (let i = 0; i < MAX_CALLS; i++) {
